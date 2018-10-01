5a80e06d26d25ce0ad3e5e28dc21cc4e1246f1d0
Micah Dameron <micahdameron@gmail.com>
2018-10-01 06:13:06 -0400

    modified:   README.md 	modified:   laughing-spoon.sh

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ A bash script for scoping Solidity code bases during the pre-assessment phase of
 
 - Automatically exclude lines in files we've audited before or that don't need scoped for some other reason.
 - Apply some more cool automatic searching/sorting of .sol files to the generic output.
+- Report as an MD file instead of a plaintext outfile
--- laughing-spoon.sh
+++ laughing-spoon.sh
@@ -6,7 +6,7 @@ normal=$(tput sgr0)
 currentDate=`date`
 #ONCE THESE VARIABLES ARE SET, BOLD/UNBOLD "TEXT" BY PUTTING ${bold}"TEXT"${normal}
 echo "Checking dependencies..."
-#sleep 0.5
+sleep 0.5
 
 #echo -e "\033[4mThis is underlined text.\033[0m"
 
@@ -21,9 +21,9 @@ surryaa() {
 	exit
     fi
 }
-#sleep 0.5
+sleep 0.5
 surryaa
-#sleep 0.3
+sleep 0.3
 
 graffViz() {
     if command -v dot --help>/dev/null; then
@@ -33,62 +33,62 @@ graffViz() {
 	exit
     fi
 }
-#sleep 0.4
+sleep 0.4
 
-#sleep 0.9
+sleep 0.9
 graffViz 
-#sleep 0.5
+sleep 0.5
 
-echo -e "\033[4mWelcome to laughing-spoon, a minimally-interactive bash script that automates pre-assessment (scoping) phase of security audits on Solidity code bases.\033[0m
+echo -e "Welcome to laughing-spoon, a minimally-interactive bash script that automates pre-assessment (scoping) phase of security audits on Solidity code bases.
 "
-#sleep 2
+sleep 2
 
 echo "Shell directory: ${bold}$BASH${normal}"
-#sleep .5
+sleep .5
 echo "Shell version: ${bold}$BASH_VERSION${normal}"
-#sleep .5
+sleep .5
 echo "Home directory: ${bold}$HOME${normal}"
-#sleep .5
+sleep .5
 echo "Working directory: ${bold}$PWD${normal}
 "
-#sleep 1.5
+sleep 1.5
 echo "${bold}laughing-spoon.sh${normal} needs to point to the root directory of your smart contract system.
 "
-#sleep 1
+sleep 1
 
 read -e -p "Please enter the directory of the contract system to be assessed: " contractRoot
 
 echo ${bold}"Counting Solidity files."${normal}
-#sleep 0.5
+sleep 0.5
 echo "Number of Solidity files in codebase: " > scopingReport.out
 find $contractRoot -name '*.sol' | wc -l >> scopingReport.out
 echo " " >> scopingReport.out                                                                                           #Creates a visual line break in the report output.
 echo ${bold}"Solidity file count saved to ./scopingReport.out"${normal}
-#sleep 0.5
+sleep 0.5
 
 echo ${bold}"Counting lines of Solidity Code."${normal}
-#sleep 0.5
+sleep 0.5
 find $contractRoot -name '*.sol' | xargs wc -l >> scopingReport.out
 echo " " >> scopingReport.out                                                                                           #Creates a visual line break in the report output.
 echo ${bold}"Solidity line count saved to ./scopingReport.out"${normal}
-#sleep 0.5
+sleep 0.5
 
 echo "Analyzing the number and nature of functions with ${bold}Surya${normal}"
 surya describe $contractRoot**/*.sol >> scopingReport.out 
 echo "Surya description saved to ./scopingReport.out"
-#sleep 0.1
+sleep 0.1
 
 echo "Creating inheritance graph with ${bold}Surya${normal}"
-#sleep 0.5
+sleep 0.5
 surya inheritance $contractRoot**/*.sol | dot -T png > inheritanceGraph.png
 echo "Inheritance graph generated. Saved to ./inheritanceGraph.png"
-#sleep 0.1
+sleep 0.1
 
 echo "Generating call graph with ${bold}Surya${normal}"
-surya callgraph $contractRoot**/*.sol | dot -T png > callGraph.png
-#sleep 0.5
+surya graph $contractRoot**/*.sol | dot -T png > callGraph.png
+sleep 0.5
 echo "Call graph generated. Saved to callGraph.png"
-#sleep 0.5
+sleep 0.5
 
 echo "
 Scoping complete. Results printed to scopingReport.out, inheritanceGraph.png, and callGraph.png"
