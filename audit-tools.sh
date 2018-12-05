40fedb8649b6bf89b96e1540c2ce004b5a0f9a9a
Micah Dameron <micahdameron@gmail.com>
2018-12-05 13:14:27 -0500

    modified:   audit-tools.sh 	modified:   .gitignore

--- audit-tools.sh
+++ audit-tools.sh
@@ -8,7 +8,7 @@ shopt -s globstar
 
 dialogCheck() {
     if command -v dialog --help>/dev/null; then
-        printf | dialog --clear --gauge "Dependency dialog passed." 10 70 3
+        printf | dialog --gauge "Dependency dialog passed." 10 70 3
     else
         printf "Dependency dialog not passed. Install dialog to continue. For help installing, read the ${bold}Dependencies${normal} section in the README." 
       exit
@@ -17,7 +17,7 @@ dialogCheck() {
 
 suryaCheck() {
     if command -v surya --help>/dev/null; then
-        printf | dialog --clear --gauge "Dependency surya passed." 10 70 6
+        printf | dialog --gauge "Dependency surya passed." 10 70 6
     else
         printf "Dependency Surya not passed. Install Surya to continue. For help installing, read the ${bold}Dependencies${normal} section in the README." 
       
@@ -25,10 +25,9 @@ suryaCheck() {
     fi
 }
 
-
 graphvizCheck() {
     if command -v dot --help>/dev/null; then
-        printf | dialog --clear --gauge "Dependency graphviz passed." 10 70 9
+        printf | dialog --gauge "Dependency graphviz passed." 10 70 9
     else
         printf | dialog --colors --title "ERROR" \
                       --yesno "\ZbGraphviz\Zn was not found on this system. Scoping requires \Zbgraphviz\Zn to run. Instructions for installing, \Zbgraphviz\Zn can be found in the README. Would you like to view this section of the README?" 10 70
@@ -45,12 +44,12 @@ esac
 
 getDir() {
     rootDir=$(dialog --title "Root Contract Directory" --stdout --title "Select the root contract directory and press spacebar then enter." --dselect ~/ 10 70)       
-        printf | dialog --clear --gauge "Root directory acquired." 10 70 12
+        printf | dialog --gauge "Root directory acquired." 10 70 12
 }
 
 createReport() {
 printf "# Scoping Report \n" > ScopingReport.md   
-        printf | dialog --clear --gauge "Scoping report file created." 10 70 15
+        printf | dialog --gauge "Scoping report file created." 10 70 15
 
 }
 
@@ -90,7 +89,7 @@ fileCount() {
     fF=$(filterFind | wc -l)
   printf "* **$uF** Solidity files exist in this contract system.\n" >> ScopingReport.md
   printf "* but, only **$fF** of those need audited.\n" >> ScopingReport.md
-  printf | dialog --clear --gauge "File count summed and written to ScopingReport.md" 10 70 18
+  printf | dialog --gauge "File count summed and written to ScopingReport.md" 10 70 18
 
 }
 
@@ -101,7 +100,7 @@ lineCount() {
         fF2=$(filterFind | xargs wc -l | tail -1 | sed -e 's/total//g' | sed -e 's/^[[:space:]]*//')
     printf "* **$uF2** Solidity lines exist in this contract system.\n" >> ScopingReport.md
     printf "* but, only **$fF2** of those need audited.\n" >> ScopingReport.md
-    printf | dialog --clear --gauge "Line count summed and written to ScopingReport.md" 10 70 21
+    printf | dialog --gauge "Line count summed and written to ScopingReport.md" 10 70 21
 }
 
 
@@ -110,46 +109,43 @@ suryaMdReport() {
     printf "**Surya's Markdown Report** gives an eagle-eye view of a smart contract system's inner workings. It displays:\n"  >> ScopingReport.md
     printf "* Files to be audited, along with their SHA-1 hash. \n" >> ScopingReport.md
     printf "* The directory of each file in the contract system. \n" >> ScopingReport.md
-    printf "* The description table of all contracts (`surya describe`.) \n" >> ScopingReport.md
-      surya mdreport mdreport.md $suryaFilter > tmp.txt
-    printf | dialog --clear --gauge "Markdown Report created." 10 70 24
-      cat mdreport.md >> ScopingReport.md && printf "\n" >> ScopingReport.md
-    printf | dialog --clear --gauge "Markdown Report written." 10 70 27
-      rm tmp.txt mdreport.md
-    printf | dialog --clear --gauge "Temporary files deleted." 10 70 30
+    printf "* The description table of all contracts (surya describe.) \n" >> ScopingReport.md
+    printf "* Click (here)[MDReport.md] to view Surya's Markdown Report. \n" >> ScopingReport.md
+      surya mdreport MDReport.md $suryaFilter 
+    printf | dialog --gauge "Markdown Report written." 10 70 27
 }
 
-suryaGraph() {
+suryaCall() {
   printf "## Call Graph\n" >> ScopingReport.md
   printf "**Surya's Call Graph** creates an exhaustive visualization of all function calls." >> ScopingReport.md
-  surya graph $suryaFilter  | dot -T png CallGraph.png
+  surya graph $suryaFilter  | dot -Tpng CallGraph.png
   printf "![Call Graph](CallGraph.png)"
 }
 
-suryaGraph() {
-  printf "## Call Graph\n" >> ScopingReport.md
+suryaInheritance() {
+  printf "## Inheritance Graph\n" >> ScopingReport.md
   printf "**Surya's Inheritance Graph** creates an exhaustive visualization of all function calls." >> ScopingReport.md
-  surya inheritance $suryaFilter  | dot -T png InheritanceGraph.png
+  surya inheritance $suryaFilter  | dot -Tpng InheritanceGraph.png
   printf "![Inheritance Graph](InheritanceGraph.png)"
 }
 
 createAppendix() {
   printf "# Appendices\n" >> ScopingReport.md
-  printf | dialog --clear --gauge "Appendix written." 10 70 33
+  printf | dialog --gauge "Appendix written." 10 70 33
 }
 
 
 suryaDescribe() {
     printf "## Appendix A: Surya\n" >> ScopingReport.md
     printf "### Surya Describe\n" >> ScopingReport.md
-    surya describe $suryaFilter | sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g" >> ScopingReport.md
-    printf | dialog --clear --gauge "Description Report Written" 10 70 36
+    surya describe $suryaFilter #| sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g" >> ScopingReport.md
+    printf | dialog --gauge "Description Report Written" 10 70 36
 }
 
 suryaParse(){
     printf "### Surya Parse\n" >> ScopingReport.md
     surya parse $suryaFilter >> ScopingReport.md
-    printf | dialog --clear --gauge "Parse Report Written" 10 70 39
+    printf | dialog --gauge "Parse Report Written" 10 70 39
 }
 
 
@@ -172,7 +168,7 @@ CHOICE=$(dialog --colors --aspect 0 --begin 3 65 --title "Audit Tools" --infobox
 "\Z1Shell directory:\Zn \Zb$BASH$\Zn 
 \Z1Shell version:\Zn \Zb$BASH_VERSION$\Zn 
 \Z1Working directory:\Zn \Zb$PWD$\Zn" 5 55 \
---and-widget --clear --backtitle "$BACKTITLE" --title "$TITLE" --menu "$MENU" $HEIGHT $WIDTH $CHOICE_HEIGHT "${OPTIONS[@]}" 2>&1 >/dev/tty)
+--and-widget --backtitle "$BACKTITLE" --title "$TITLE" --menu "$MENU" $HEIGHT $WIDTH $CHOICE_HEIGHT "${OPTIONS[@]}" 2>&1 >/dev/tty)
 
 
 case $CHOICE in
@@ -185,9 +181,13 @@ case $CHOICE in
         createReport
         fileCount
         lineCount
+        suryaDescribe
+        suryaInheritance
+        suryaCall
+        suryaParse
         suryaMdReport
         createAppendix
-        suryaDescribe
+        
 
             ;;
         2)
